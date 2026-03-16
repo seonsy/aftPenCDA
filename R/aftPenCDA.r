72926daaac83a418dfa859f6076e96ac7701e30b
Suyeon Seon <seonsy429@gmail.com>
2026-03-16 13:41:49 +0900

    update aftpen implementation, add match.arg handling, and add PIC version function

--- R/aftPenCDA.r
+++ R/aftPenCDA.r
@@ -1,4 +1,4 @@
-#' @export
+
 BAR_threshold <- function(resid, lambda, n) {
   if (resid <= 2 * sqrt(lambda / n) && resid >= -2 * sqrt(lambda / n)) {
     0
@@ -6,7 +6,7 @@ BAR_threshold <- function(resid, lambda, n) {
     resid / 2 + sqrt(resid^2 / 4 - lambda / n)
   }
 }
-#' @export
+
 Soft <- function(z, lambda) {
   dplyr::case_when(
     z >  lambda ~ z - lambda,
@@ -16,7 +16,8 @@ Soft <- function(z, lambda) {
 }
 
 #' @export
-aftPenCDA <- function(dt, lambda, se, type, r = 3.7, eps = 1e-8, max.iter = 100) {
+aftpen <- function(dt, lambda, se, type = c("BAR", "LASSO", "ALASSO", "SCAD"), r = 3.7, eps = 1e-8, max.iter = 100) {
+  type <- match.arg(type)
   fit  <- is_aft_cpp(dt$y, dt$d, as.matrix(dt[, -c(1, 2)]), se)
   init <- fit$beta
   h    <- fit$hess
@@ -36,7 +37,7 @@ aftPenCDA <- function(dt, lambda, se, type, r = 3.7, eps = 1e-8, max.iter = 100)
 
   if (type == "BAR") {
     beta <- init
-    tilde_beta <- init
+    beta_prev <- init
     resid <- u - z %*% beta
 
     for (t in seq_len(max.iter)) {
@@ -49,24 +50,24 @@ aftPenCDA <- function(dt, lambda, se, type, r = 3.7, eps = 1e-8, max.iter = 100)
       }
 
       if (max(abs(beta - new.beta)) < eps) break
-      tilde_beta <- beta
+      beta_prev <- beta
       beta <- new.beta
     }
 
     return(list(
       beta  = beta / sqrt(norm.z),
-      tbeta = tilde_beta / sqrt(norm.z)
+      tbeta = beta_prev / sqrt(norm.z)
     ))
   }
 
-  if (type == "ALASSO") tilde.beta <- init
-  if (type == "LASSO")  tilde.beta <- rep(1, length(init))
+  if (type == "ALASSO") beta_weight <- init
+  if (type == "LASSO")  beta_weight <- rep(1, length(init))
 
   init2 <- coef(lm(u ~ -1 + z))
   init2 <- ifelse(is.na(init2), eps, init2)
 
   beta <- init2
-  tilde_beta <- init2
+  beta_prev <- init2
   resid <- u - z %*% beta
 
   for (t in seq_len(max.iter)) {
@@ -81,19 +82,104 @@ aftPenCDA <- function(dt, lambda, se, type, r = 3.7, eps = 1e-8, max.iter = 100)
           (2 * lambda < abs(zj) & abs(zj) <= r * lambda) +
           zj * (abs(zj) > r * lambda)
       } else {
-        Soft(zj, lambda / abs(tilde.beta[j]))
+        Soft(zj, lambda / abs(beta_weight[j]))
       }
 
       resid <- resid - z[, j] * (new.beta[j] - beta[j])
     }
 
     if (max(abs(beta - new.beta)) < eps) break
-    tilde_beta <- beta
+    beta_prev <- beta
     beta <- new.beta
   }
 
   list(
     beta  = beta / sqrt(norm.z),
-    tbeta = tilde_beta / sqrt(norm.z)
+    tbeta = beta_prev / sqrt(norm.z)
+  )
+}
+
+#' @export
+aftpen_pic<- function(dt, lambda, se,
+                         type = c("BAR", "LASSO", "ALASSO", "SCAD"), r = 3.7, eps = 1e-8, max.iter = 100) {
+  type <- match.arg(type)
+  fit  <- is_aftp_cpp(dt, se)
+  init <- fit$beta
+  h    <- fit$hess
+  g    <- fit$grad
+
+  x_chol <- chol(h)
+  y_tilde <- forwardsolve(t(x_chol), h %*% init - g)
+
+  n <- length(y_tilde)
+  p <- ncol(x_chol)
+
+  u <- y_tilde - mean(y_tilde)
+
+  z <- t(t(x_chol) - apply(x_chol, 2, mean))
+  norm.z <- apply(z^2, 2, mean)
+  z <- t(t(z) / sqrt(norm.z))
+
+  if (type == "BAR") {
+    beta <- init
+    beta_prev <- init
+    resid <- u - z %*% beta
+
+    for (t in seq_len(max.iter)) {
+      new.beta <- beta
+
+      for (j in seq_len(p)) {
+        zj <- (crossprod(z[, j], resid) / n) + beta[j]
+        new.beta[j] <- BAR_threshold(zj, lambda, n)
+        resid <- resid - z[, j] * (new.beta[j] - beta[j])
+      }
+
+      if (max(abs(beta - new.beta)) < eps) break
+      beta_prev <- beta
+      beta <- new.beta
+    }
+
+    return(list(
+      beta  = beta / sqrt(norm.z),
+      tbeta = beta_prev / sqrt(norm.z)
+    ))
+  }
+
+  if (type == "ALASSO") beta_weight = init
+  if (type == "LASSO")  beta_weight = rep(1, length(init))
+
+  init2 <- coef(lm(u ~ -1 + z))
+  init2 <- ifelse(is.na(init2), eps, init2)
+
+  beta <- init2
+  beta_prev <- init2
+  resid <- u - z %*% beta
+
+  for (t in seq_len(max.iter)) {
+    new.beta <- beta
+
+    for (j in seq_len(p)) {
+      zj <- crossprod(z[, j], resid) / n + beta[j]
+
+      new.beta[j] <- if (type == "SCAD") {
+        Soft(zj, lambda) * (abs(zj) <= 2 * lambda) +
+          Soft(zj, r * lambda / (r - 1)) / (1 - 1 / (r - 1)) *
+          (2 * lambda < abs(zj) & abs(zj) <= r * lambda) +
+          zj * (abs(zj) > r * lambda)
+      } else {
+        Soft(zj, lambda / abs(beta_weight[j]))
+      }
+
+      resid <- resid - z[, j] * (new.beta[j] - beta[j])
+    }
+
+    if (max(abs(beta - new.beta)) < eps) break
+    beta_prev <- beta
+    beta <- new.beta
+  }
+
+  list(
+    beta  = beta / sqrt(norm.z),
+    tbeta = beta_prev / sqrt(norm.z)
   )
 }
